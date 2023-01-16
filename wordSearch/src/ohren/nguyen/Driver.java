74fc08c8114e745d32755daa3112da83f0adb831
334697604 <334697604@C-MXL60108QB.tdsb.on.ca>
2023-01-16 14:54:32 -0500

    removed the commented test-cases out of the driver

--- wordSearch/src/ohren/nguyen/Driver.java
+++ wordSearch/src/ohren/nguyen/Driver.java
@@ -1,52 +1,29 @@
 package ohren.nguyen;
 
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class Driver {
 	public static void main(String[] args) {
 		/*
-		System.out.println("hello");
-		System.out.println("goodbye");
-		System.out.println("happy");
-		System.out.println("personal comp");
-
-		
 		// Creates a Letter object of each alphabet letter (uppercase)
 		for (int n = 65; n < 91; n++) {
 			Letter l = new Letter((char)n);
 			System.out.println(l);
 		}
-
 		*/
-		
-		
-		/* --- TESTING --- 
-		
-		//** WORKS 13/01/23 **\\
-		// Creates a Letter object of each alphabet letter (uppercase)
-		for (int n = 65; n < 91; n++) {
-			Letter l = new Letter((char)n);
-			System.out.println(l);
-		}
-		
-		//** WORKS 13/01/23 **\\
-		// Creates random letters
-		Letter r = new Letter();
-			System.out.println(r);
-		Letter r2 = new Letter();
-			System.out.println(r2);
-		Letter r3 = new Letter();
-			System.out.println(r3);
-		
-		--- TESTING END --- */ 
-		
-		Letter r = new Letter();
-			System.out.println(r);
-		
-		String name = "zalea";
-		String name2 = "ohren";
-		char c = r.getChar();
-		System.out.println(name + name2);
-		System.out.println(name + c);
 
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("WORD SEARCH GENERATOR");
+		System.out.println("---------------------");
+		System.out.println("How ");
 	}
 }
