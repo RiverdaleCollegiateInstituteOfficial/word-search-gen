a4e06180d50682f50f552df5a8e9c1553f6027e4
334697604 <334697604@C-MXL60108QB.tdsb.on.ca>
2023-01-16 15:13:10 -0500

    worked on file opening stuff in driver, added Wordlist class

--- wordSearch/src/ohren/nguyen/Driver.java
+++ wordSearch/src/ohren/nguyen/Driver.java
@@ -3,10 +3,12 @@ package ohren.nguyen;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class Driver {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		/*
 		// Creates a Letter object of each alphabet letter (uppercase)
 		for (int n = 65; n < 91; n++) {
@@ -14,12 +16,16 @@ public class Driver {
 			System.out.println(l);
 		}
 		*/
+		
+		File file = new File("list_file.txt");
+		Scanner in = new Scanner(file);
 
-		
-		
-		
-		
-		
+		ArrayList<String> words = new ArrayList<>();
+		/**
+		while (in.hasNext()) {
+			System.out.println(in.nextLine());
+		}
+		**/
 		
 		
 		System.out.println("WORD SEARCH GENERATOR");
